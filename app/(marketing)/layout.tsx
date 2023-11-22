3d6abde22def0fb0c1e1724529285b82a61d660c
WNagorny <walery.nagorny@gmail.com>
2023-11-22 08:35:41 +0100

    created a Footer component for Marketing Page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar } from './_components/navbar';
+import { Footer } from './_components/footer';
 
 const MarketingLayout = ({
    children
@@ -13,7 +14,7 @@ const MarketingLayout = ({
       {children}
       </main>
 
-      {/* Footer */}
+      <Footer/>
 
    </div>
   )
